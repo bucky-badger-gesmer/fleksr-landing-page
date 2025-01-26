7af662ef40032dcc7b2f6f655c1f45b56f436add
bucky-badger-gesmer <aaron.gesmer@gmail.com>
2025-01-26 16:34:45 -0600

    chore: touch up some copy

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -6,11 +6,11 @@ export default function Hero() {
             <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
               <div className="text-center">
                 <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                  <span className="block xl:inline">Rent and Share High-End Gym</span>{" "}
+                  <span className="block xl:inline">Rent and Share Premium Gym</span>{" "}
                   <span className="block text-indigo-600 xl:inline">Memberships</span>
                 </h1>
                 <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-3xl sm:mx-auto md:mt-5 md:text-xl lg:mx-auto">
-                  Access premium gyms without the long-term commitment. fleksr lets you rent high-end gym memberships,
+                  Access premiere facilities without the long-term commitment. With fleksr, you can rent high-end gym meberships,
                   giving you flexibility and variety in your fitness journey.
                 </p>
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-center">
